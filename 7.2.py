2d8ea70b1d073600ab7eab155e59d94d212a75aa
tativ <thomas.thornhill@yale.edu>
2018-03-29 16:05:16 -0400

    update

---
+++ 7.2.py
@@ -0,0 +1,8 @@
+"""
+The number of participants that survived beyond 5 years in a cohort of 𝑁 participants
+follows a binomial distribution.
+
+Parameters
+N = The number of patients = 573
+p ∈ [0,1]
+"""
