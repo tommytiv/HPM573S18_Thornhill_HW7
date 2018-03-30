71fe6d4505b1777dcbd1d991ee737845e4ec650e
tativ <thomas.thornhill@yale.edu>
2018-03-30 00:37:21 -0400

    update

---
+++ 7.3.py
@@ -0,0 +1,13 @@
+from scipy.stats import binom
+
+# Parameters for a binomial probability mass function
+x = 400     # x = total number of “successes”
+n = 573     # n = number of trials
+p = 0.50    # p = probability of a success on an individual trial
+
+Probability = binom._pmf(x,n,p)
+
+print ("The likelihood that a clinical study reports 400 of 573 "
+       "participants survived at the end of the 5-year study period "
+       "if 50% of the patients in our simulated cohort survived beyond 5 years is ",
+       Probability)
