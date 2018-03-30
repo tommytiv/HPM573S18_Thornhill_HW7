0fffb76a1e5c31da28e45e20dc610523be85555e
tativ <thomas.thornhill@yale.edu>
2018-03-30 13:22:44 -0400

    update

--- 7.3.py
+++ 7.3.py
@@ -1,13 +1,16 @@
 from scipy.stats import binom
+from scipy.stats import bernoulli
 
 # Parameters for a binomial probability mass function
 x = 400     # x = total number of “successes”
 n = 573     # n = number of trials
-p = 0.50    # p = probability of a success on an individual trial
+p = 0.5724258289703316    # p = probability of a success on an individual trial
 
 Probability = binom._pmf(x,n,p)
-
-print ("The likelihood that a clinical study reports 400 of 573 "
-       "participants survived at the end of the 5-year study period "
-       "if 50% of the patients in our simulated cohort survived beyond 5 years is ",
-       Probability)
+Probability2 = binom._pmf(x-328,n,p)
+#print ("The likelihood that a clinical study reports 400 of 573 "
+#      "participants survived at the end of the 5-year study period "
+ #      "if 50% of the patients in our simulated cohort survived beyond 5 years is ",
+#       Probability)
+print (Probability)
+print (Probability2)
